2d7bb0b5cd133d8b79cf27856630133c1727010d
Lee khang <45526376+admincheck123@users.noreply.github.com>
2024-09-15 23:16:18 +0700

    Add files via upload

---
+++ scripts_tkb.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Hiện màn hình tải trong 2 giây
+    setTimeout(() => {
+        const loadingScreen = document.getElementById('loading-screen');
+        if (loadingScreen) {
+            loadingScreen.classList.add('hidden');
+        }
+    }, 2000);
+
+    // Intersection Observer code
+    const sections = document.querySelectorAll('.day');
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
